920ab712bfff4e346197c00b66a11936ff17aafd
Tyler Wooldridge <twooldridge@boslogin04.rc.fas.harvard.edu>
2020-11-04 09:43:38 -0500

    Changes to vcf2popcounts.py

--- vcf2popcounts.py
+++ vcf2popcounts.py
@@ -7,7 +7,9 @@ import os
 import argparse
 import tempfile as tmp
 
-def process_vcf(vcf_path,sample_map,fraction_missing=None,maf=None):
+def process_vcf(vcf_path, sample_map, allelecounts, fraction_missing=None, maf=None):
+    
+    ## Grab the vcf
     if '.vcf.gz' in vcf_path:
         opener = gzip.open
     elif '.vcf' in vcf_path:
@@ -15,42 +17,53 @@ def process_vcf(vcf_path,sample_map,fraction_missing=None,maf=None):
     else:
         print('file type not recognized')
     file = opener(vcf_path, 'rt')
+    
+    ## Setup data structures that will record counts, etc.
     pops = set(sample_map.values())
     snp_pop_counts = []
     pop_counts = dict.fromkeys(pops)
     header = list(pop_counts.keys())
-    #snp_pop_counts.append('chr\tpos\tref\talt\t' + '\t'.join(header))
     num_alleles = len(sample_map.keys())*2
+    
+    ## Begin processing file line by line
+    last_reported = 0
     for l in file:
-        # Get sample info line as list
+        
+        ##  If you hit the header line, grab the sample names and store the order in which they appear in the vcf
         if re.match("#CHROM",l):
             s = l.strip('\n')
             s = s.split('\t')
             vcf_sample_order = s[9:]
             pop_order = {}
+            ## This is setup so that if a sample is in the vcf but not specified 
+            ## in the supplied pop map (not of interest), then it's fine
             for idx,sample in enumerate(vcf_sample_order):
                 if sample in sample_map.keys():
                     pop_order[idx] = sample_map[sample]
                 else:
                     continue
+            ## Sanity check
             if len(pop_order) != len(sample_map):
                 sys.exit("Error obtaining pop order from vcf, exiting")
-            #pop_order = [sample_map[sample] for sample in sample_order]
-            #print(sample_order)
-            #print(pop_order)
             print("Sample order obtained...")
             continue
-        # If 'l' is an info line, skip
+        
+        ## Skip info lines, start processing when you hit lines without '#'
         if not re.match("#", l):
             d = re.split('\t', l)
             pos = d[1]
+            ## Progress report every ~1e6 BP
+            if (((int(pos) % 1e6) < 50000) and ((int(pos) - last_reported) >= 1e6)):
+                print('Processing %s:%s' %(d[0],pos))
+                last_reported = int(pos)
             ref = [d[3]]
             alt = re.split(",", d[4])
             alleles = ref + alt
+            
+
             # If polyallelic, skip to next site now. Only interested in biallelic sites
             #if len(alleles) > 2:
                 #continue
-
             # Handle (skip) indels
             #indel = False
             #for allele in alleles:
@@ -60,58 +73,93 @@ def process_vcf(vcf_path,sample_map,fraction_missing=None,maf=None):
                     #if allele != "<NON_REF>":
                     #    indel = True
 
-            # Now get genotype info for snps
+            
+            ## Now get genotype info for snps
             genotypes = d[9:]
-            # Subest for the samples we're interested in
+            ## Grabbing the genotypes only for the samples we're interested in
             genotypes = [genotypes[i] for i in pop_order.keys()]
+            ## This gets rid of the depth & QC info
             genotypes = [(i.split(':')[0]) for i in genotypes]
+            ## This splits up diploid genotypes so that I can count the # of alleles. Will work for haploid as well
             genotypes = [re.split('/|\|',i) for i in genotypes]
+            ## Formatting
             basic_genotypes = flatten(genotypes)
-            # Do some filtering
+            
+
+            ## Now we do some filtering
+            ## Missing data
             if fraction_missing is not None:
                 num_missing = basic_genotypes.count('.')
                 if (num_missing/num_alleles) > float(fraction_missing):
                     continue
+            ## Minor allele frequency
             if maf is not None:
                 num_minor = basic_genotypes.count('1')
                 num_genotyped = num_minor + basic_genotypes.count('0')
             if float(num_genotyped) == 0: continue    
             if (num_minor/float(num_genotyped)) < float(maf):
                     continue
-            #genotypes = [[int(float(j)) for j in i] for i in genotypes]
+            
+
+            ## This is where the work actually happens
+            ## Dictionary to store allele counts, can handle multiallelic sites
             pop_counts = dict(zip(pops,[[[0],[0] * (len(alleles)-1)] for i in range(len(pops))]))
+            ## List of pop names that is the same length as the # of samples being processed
             pop_list = list(pop_order.values())
             for i,gen in enumerate(genotypes):
+                ## Skip missing
                 if '.' in gen:
                     continue
                 current_pop = pop_list[i]
-                #print(current_pop)
-                #print(sample_order[i])
-                #if len(alleles) > 2:
+                ## Initialize 
                 num_ref = [0]
                 num_alt = [0] * (len(alleles)-1)
+                ## The lines below do the actualy counting
                 for allele in gen:
                     if allele == '0':
                         num_ref[0] = num_ref[0] + 1
                     else:
                         num_alt[int(allele)-1] = num_alt[int(allele)-1] + 1
-                    #if allele == '1':
-                    #    num_alt = num_alt + 1
-                #print(d[0])
-                #print(pos)
-                #print(alleles)
-                #print(pop_counts[current_pop][0])
-                #print(num_ref)
+                ## Store results
                 pop_counts[current_pop][0] = [a+b for a,b in zip(pop_counts[current_pop][0],num_ref)]
                 pop_counts[current_pop][1] = [a+b for a,b in zip(pop_counts[current_pop][1],num_alt)]           
             
+            ## BTW, write header, if this hasn't been done already
             if len(snp_pop_counts) == 0:
-                snp_pop_counts.append('chr\tpos\tref\talt\t' + '\t'.join(pop_counts.keys()))
+            	if allelecounts:
+                	snp_pop_counts.append('chr\tpos\tref\talt\t' + '\t'.join([i + '_2n' for i in pop_counts.keys()]) + '\t' + '\t'.join([i + '_raw_alts' for i in pop_counts.keys()]) + '\t' + '\t'.join([i + '_freqs' for i in pop_counts.keys()]))
+            	else:
+            		snp_pop_counts.append('chr\tpos\tref\talt\t' + '\t'.join([i + '_2n' for i in pop_counts.keys()]) + '\t' + '\t'.join([i + '_freqs' for i in pop_counts.keys()]))
+
+
+            ## Now parse the count data, calculate frequencies, etc. 
+            ## The below lines produce a nested list structure like [[1,1], [0,1]] where each nested bracket is a population
             pop_counts = list(pop_counts.values())
             pop_counts = [flatten(l) for l in pop_counts]
-            pop_counts = [','.join(map(str, i)) for i in pop_counts]
-            pop_counts = '\t'.join(pop_counts)
-            snp_pop_counts.append('\t'.join([d[0],pos,ref[0],','.join(alt),pop_counts]))
+            
+            ## Frequencies, accounting for how many alleles are actually called in the population
+            altfreqs = []
+            for popdata in pop_counts:
+            	ff = []
+            	for altallele in popdata[1:]:
+            		ff.append(altallele/sum(popdata))
+            	altfreqs.append(ff)
+            altfreqs = [','.join(map(str, i)) for i in altfreqs]
+            altfreq_string = '\t'.join(altfreqs)
+            ## Get 2n value (How many alleles were sequenced?)
+            pop_2n = [sum(popdata) for popdata in pop_counts]
+            pop_2n = '\t'.join([str(i) for i in pop_2n])
+
+            ## Save data to list that will be written to file
+            if allelecounts:
+                ## If you also want the raw allele counts, we do the formatting and storing here
+                pop_counts = [d[1:] for d in pop_counts]
+                pop_counts = [','.join(map(str, i)) for i in pop_counts]
+                pop_string = '\t'.join(pop_counts)
+            	snp_pop_counts.append('\t'.join([d[0], pos, ref[0],  ','.join(alt), pop_2n,pop_string,altfreq_string]))
+            else:
+                ## This line is if you just want frequencies
+            	snp_pop_counts.append('\t'.join([d[0], pos, ref[0],  ','.join(alt), pop_2n,altfreq_string]))
     
     return(snp_pop_counts)
 
@@ -123,6 +171,8 @@ def main():
     parser.add_argument("--map", help="popmap file -- this is a 2-column, tab separated file with sample names in the first column, and the corresponding population in the second")
     parser.add_argument("--fraction_missing",help="fraction of genotypes that can be missing before a site is filtered out. 0 = no missing genotypes allowed, 1 = all missing genotypes allowed. Default is None")
     parser.add_argument("--maf",help="filter variants by minor allele frequency. Default = 0.025")
+    #parser.add_argument('--freq', help ="output population frequencies in addition to raw allele counts", default=True, action='store_true')
+    parser.add_argument('--counts', help ="output raw allele counts in addition to frequences", default=False, action='store_true')
     parser.add_argument("--out", help="path to output file")
     
 
@@ -132,20 +182,22 @@ def main():
     map_file = args.map
     missing_cutoff = args.fraction_missing
     maf = args.maf
+    counts = args.counts
     if maf is None:
         maf_val = 0.025
     else:
         maf_val = maf
     outfile = args.out
 
-    # Run
+    ## Run
     samplemap = get_samplemap(map_file)
     print("Processing vcf...")
-    treemix_lines = process_vcf(vcf,samplemap,fraction_missing = missing_cutoff,maf = maf_val)
+    ## This is the bulk of the work
+    count_lines = process_vcf(vcf, samplemap, counts, fraction_missing = missing_cutoff, maf = maf_val)
 
-    # Write
+    ## Write file
     out = open(outfile, 'w')
-    for line in treemix_lines:
+    for line in count_lines:
         writeable = line
         out.write('%s\n' % (writeable))
 
--- vcf_filter_pipeline.sh
+++ vcf_filter_pipeline.sh
@@ -21,10 +21,11 @@ module load java
 module load bcftools
 #export PATH="/n/home01/lassance/Software/bcftools-1.7/:$PATH"
 export BCFTOOLS_PLUGINS=/n/home01/lassance/Software/bcftools-1.7/plugins
+ZIPCMD="/n/home11/twooldridge/scripts/misc/zip_index_vcf.sh"
 
 ### VARIANT ###
 java -Xmx10G -XX:ParallelGCThreads=1 -Djava.io.tmpdir=`pwd`/tmp -jar ~/Software/GATK/GenomeAnalysisTK-3.8.jar -T SelectVariants -R ${refgenome} -V ${invcf} --selectTypeToExclude NO_VARIATION -o ${sampleID}.var.vcf
-bash /n/home11/twooldridge/scripts/zip_index_vcf.sh ${sampleID}.var.vcf
+\$ZIPCMD ${sampleID}.var.vcf
 #
 #bcftools view -i 'ALT !~ "*" && TYPE="snp"' ${sampleID}.var.vcf.gz -O v  -o ${sampleID}.snps.vcf 
 #bcftools view -e 'ALT !~ "*" && TYPE="snp"' ${sampleID}.var.vcf.gz -O v  -o ${sampleID}.indels.vcf 
@@ -32,35 +33,40 @@ bash /n/home11/twooldridge/scripts/zip_index_vcf.sh ${sampleID}.var.vcf
 java -Xmx5G -jar ~/Software/picard/2.18.4/picard.jar SplitVcfs I=${sampleID}.var.vcf.gz SNP_OUTPUT=${sampleID}.snps.vcf INDEL_OUTPUT=${sampleID}.indels.vcf STRICT=false
 #
 java -Xmx5G -XX:ParallelGCThreads=1 -Djava.io.tmpdir=`pwd`/tmp -jar ~/Software/GATK/GenomeAnalysisTK-3.8.jar -R ${refgenome}  -T VariantFiltration --variant ${sampleID}.snps.vcf --filterExpression "QD < 2.0 || MQ < 40.0 || FS > 10.0 || ReadPosRankSum < -20.0 || SOR > 3.0 || MQRankSum < -12.5" --filterName snps_filter -o sf.${sampleID}.snps.vcf 
-~/scripts/zip_index_vcf.sh sf.${sampleID}.snps.vcf 
+\$ZIPCMD sf.${sampleID}.snps.vcf 
 #
 java -Xmx5G -XX:ParallelGCThreads=1 -Djava.io.tmpdir=`pwd`/tmp -jar ~/Software/GATK/GenomeAnalysisTK-3.8.jar -R ${refgenome}  -T VariantFiltration --variant ${sampleID}.indels.vcf --filterExpression "QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0 || SOR > 3.0" --filterName indels_filter -o sf.${sampleID}.indels.vcf 
-~/scripts/zip_index_vcf.sh sf.${sampleID}.indels.vcf 
+\$ZIPCMD sf.${sampleID}.indels.vcf 
 #
 bcftools concat --allow-overlaps sf.${sampleID}.snps.vcf.gz sf.${sampleID}.indels.vcf.gz -O v -o sf.${sampleID}.var.vcf 
 #
-~/scripts/zip_index_vcf.sh sf.${sampleID}.var.vcf 
-#
-#
+\$ZIPCMD sf.${sampleID}.var.vcf 
+
+
+
 #### INVARIANT ###
 java -Xmx10G -XX:ParallelGCThreads=1 -Djava.io.tmpdir=`pwd`/tmp -jar ~/Software/GATK/GenomeAnalysisTK-3.8.jar -T SelectVariants -R ${refgenome} -V ${invcf} --selectTypeToInclude NO_VARIATION -o ${sampleID}.invar.vcf
-bash /n/home11/twooldridge/scripts/zip_index_vcf.sh ${sampleID}.invar.vcf
+\$ZIPCMD ${sampleID}.invar.vcf
 #
 bcftools filter --include '%QUAL>=20' --soft-filter poorQual -O v -o sf.${sampleID}.invar.vcf ${sampleID}.invar.vcf.gz 
-~/scripts/zip_index_vcf.sh sf.${sampleID}.invar.vcf 
+\$ZIPCMD sf.${sampleID}.invar.vcf 
 #
 #
+
+
 #### COMBINE AND DEPTH MASK ###
 bcftools concat --allow-overlaps sf.${sampleID}.var.vcf.gz sf.${sampleID}.invar.vcf.gz -O v -o sf.${sampleID}.vcf 
-bash ~/scripts/zip_index_vcf.sh sf.${sampleID}.vcf
+\$ZIPCMD sf.${sampleID}.vcf
 #
 bcftools +setGT sf.${sampleID}.vcf.gz -- -t q -e 'FMT/DP>4' -n . | bcftools view -f .,PASS > hf.${sampleID}.vcf 
-bash ~/scripts/zip_index_vcf.sh hf.${sampleID}.vcf
+\$ZIPCMD hf.${sampleID}.vcf
+
+
 
 
 ### GET SNPS AND MONOMORMPHIC SITES### 
 bcftools view hf.${sampleID}.vcf.gz -i 'ALT !~ "*"  && STRLEN(REF) == 1 && (TYPE="snp" || ALT == ".")' -M2 -O v -o ${sampleID}.snp.mono.vcf 
-bash ~/scripts/zip_index_vcf.sh ${sampleID}.snp.mono.vcf 
+\$ZIPCMD ${sampleID}.snp.mono.vcf 
 EOF
 
 sbatch ${sampleID}.vcffilter.slurm
